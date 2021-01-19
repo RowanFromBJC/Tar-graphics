94ad80e39015185d22341ee9448cf4838017a1d9
El Rowan <72898533+RowanFromBJC@users.noreply.github.com>
2021-01-19 09:57:05 -0800

    Create msi.js

---
+++ msi.js
@@ -0,0 +1,26 @@
+import { MSICreator } from 'electron-wix-msi';
+
+// Step 1: Instantiate the MSICreator
+const msiCreator = new MSICreator({
+  appDirectory: './app-dist/',
+  description: 'Easily compress and decompress files',
+  exe: 'tar-graphics-win',
+  name: 'Tar-graphics',
+  manufacturer: 'Rowan C',
+  version: '1.0.0',
+  outputDirectory: './'
+});
+
+// Step 2: Create a .wxs template file
+const supportBinaries = await msiCreator.create();
+
+// 🆕 Step 2a: optionally sign support binaries if you
+// sign you binaries as part of of your packaging script
+supportBinaries.forEach(async (binary) => {
+  // Binaries are the new stub executable and optionally
+  // the Squirrel auto updater.
+  await signFile(binary);
+});
+
+// Step 3: Compile the template to a .msi file
+await msiCreator.compile();
